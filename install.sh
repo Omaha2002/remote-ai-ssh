7d67cf5b2a0fffe0ae4c25784c9bba1503b54885
JeroenBNC <omaha2002@gmail.com>
2025-08-20 20:48:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-curl -fsSL https://raw.githubusercontent.com/<jouw-account>/<repo>/main/remote_install_user_home.sh | bash
+curl -fsSL https://raw.githubusercontent.com/Omaha2002/remote-ai-ssh/main/remote_install_user_home.sh | bash
